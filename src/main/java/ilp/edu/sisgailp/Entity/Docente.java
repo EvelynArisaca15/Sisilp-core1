e54ae80bcadb0b51d5ddb83171a283c91fd5250a
EvelynArisaca15 <evelynfer1506@gmail.com>
2022-04-27 17:51:04 -0500

    "xd"

--- src/main/java/ilp/edu/sisgailp/Entity/Docente.java
+++ src/main/java/ilp/edu/sisgailp/Entity/Docente.java
@@ -1,14 +1,57 @@
 package ilp.edu.sisgailp.Entity;
 
 import javax.persistence.Entity;
+import javax.persistence.PrimaryKeyJoinColumn;
 import javax.persistence.Table;
+import java.util.Date;
 
 @Entity
 @Table(name = "docente")
+@PrimaryKeyJoinColumn(referencedColumnName = "IDPERSONA")
+public class Docente extends Persona{
 
-public class Docente {
+    private String codDocente;
+    private Double horasAsignadas;
 
-    private String codigodocente;
-    private String cursos;
-    private int horas;
+    //CONSTRUCTOR VACIO
+    public Docente() {
+
+    }
+
+    //CONSTRUCTOR
+    public Docente(String codDocente, Double horasAsignadas) {
+        this.codDocente = codDocente;
+        this.horasAsignadas = horasAsignadas;
+    }
+
+    public Docente(Long idpersona, String codDocente, Double horasAsignadas) {
+        super(idpersona);
+        this.codDocente = codDocente;
+        this.horasAsignadas = horasAsignadas;
+    }
+
+    public Docente(String nombre, String apellido, int edad, String dni, Date fechaNacimiento, String genero, String codDocente, Double horasAsignadas) {
+        super(nombre, apellido, edad, dni, fechaNacimiento, genero);
+        this.codDocente = codDocente;
+        this.horasAsignadas = horasAsignadas;
+    }
+
+    //GET AND SET
+    public String getCodDocente() {
+        return codDocente;
+    }
+
+    public void setCodDocente(String codDocente) {
+        this.codDocente = codDocente;
+    }
+
+    public Double getHorasAsignadas() {
+        return horasAsignadas;
+    }
+
+    public void setHorasAsignadas(Double horasAsignadas) {
+        this.horasAsignadas = horasAsignadas;
+    }
+
+
 }
--- src/main/java/ilp/edu/sisgailp/Entity/Persona.java
+++ src/main/java/ilp/edu/sisgailp/Entity/Persona.java
@@ -15,11 +15,93 @@ public class Persona {
     @Column(name = "idpersona", length = 100)
     private Long idpersona;
 
-    @Column(name = "nombre", length = 100)
     private String nombre;
+    @Column(name = "nombre", length = 100, nullable = false)
+    //Como es igual al de la base de datos queda asi
     private String apellido;
     private int edad;
     private String dni;
-    private Date fechanacimiento;
-    private String genero
+
+    //Viene de la base de datos
+    @Column(name = "fecha_nacimiento", length = 50)
+    private Date fechaNacimiento;
+
+    //Constructor vacio para persona almacenar
+    public Persona() {
+
+    }
+
+    public Persona(Long idpersona) {
+        this.idpersona = idpersona;
+    }
+
+    //CONSTRUCTOR - "idpersona ya no porque se genera automatico"
+    public Persona(String nombre, String apellido, int edad, String dni, Date fechaNacimiento, String genero) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+        this.dni = dni;
+        this.fechaNacimiento = fechaNacimiento;
+        this.genero = genero;
+    }
+
+    //GET y SET
+    public Long getIdpersona() {
+        return idpersona;
+    }
+
+    public void setIdpersona(Long idpersona) {
+        this.idpersona = idpersona;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public Date getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(Date fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    private String genero;
 }
+
